90fb3befc8d06df39c8e78dbc2faa05f11ca1487
Hotel Kesari <admin@hotelkesari.com>
2026-08-19 22:30:51 +0530

    Remove compiled output from api/ directory - keep only serverless entry point

--- api/api/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("../backend/src/app");
-exports.default = (0, app_1.createApp)();
--- api/backend/src/lib/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = asyncHandler;
-/** Wraps async route handlers so thrown errors reach the error middleware. */
-function asyncHandler(fn) {
-    return (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    };
-}
